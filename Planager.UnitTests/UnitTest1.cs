e1f645e5779aff2c70a083751eba5e2a4d4abcdd
Anatoliy Gutak <anatoliy@ourtandem.com>
2021-03-15 03:10:18 +0200

    Add basic unit test

--- Planager.UnitTests/UnitTest1.cs
+++ Planager.UnitTests/UnitTest1.cs
@@ -1,17 +1,43 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Planager.API.Application.Services;
 using Planager.API.Domain.Entities;
+using Planager.UnitTests.Fakes;
+using System;
 
 namespace Planager.UnitTests
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-            var jobApp = new JobAppointment();
+        private readonly FakeLogger _logger;
 
-            
+        public UnitTest1()
+        {
+            _logger = new();
+        }
+
+        [TestMethod]
+        public void SucceedsToStartRunningJob()
+        {
+            //Arrange
+
+            var jobApp = new JobAppointment(
+                "Job 1",
+                "My job #1",
+                DateTime.UtcNow,
+                TimeSpan.Zero,
+                RepeatType.NoRepeat,
+                null,
+                "https://localhost:4444");
+
+            var job = new RunningJob(jobApp, _logger);
+
+            //Act
+            job.Start(default);
+
+            //Assert
+
+            Assert.AreEqual(JobStatus.Running, job.Status);
         }
     }
 }
